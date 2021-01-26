469ce89a413365877d43801341c2daa2d07f8e80
ccalvin97 <41994182+ccalvin97@users.noreply.github.com>
2021-01-26 11:47:58 +0800

    update

--- HRNet-Semantic-Segmentation/lib/config/default.py
+++ HRNet-Semantic-Segmentation/lib/config/default.py
@@ -22,7 +22,7 @@ _C.GPUS = (0,)
 _C.WORKERS = 1  ## workers for distributed system
 _C.PRINT_FREQ = 20
 _C.AUTO_RESUME = False
-_C.PIN_MEMORY = True
+_C.PIN_MEMORY = True ## True - dataloader不會使用虛擬記憶體, False - dataloader可以使用虛擬記憶體
 _C.RANK = 0
 
 # Cudnn related params
@@ -38,25 +38,25 @@ _C.MODEL.PRETRAINED = '' ## pre-train weight absolute address
 _C.MODEL.EXTRA = CN(new_allowed=True)
 
 _C.LOSS = CN()
-_C.LOSS.USE_OHEM = False
+_C.LOSS.USE_OHEM = False ## Ohem loss, Default is cross entropy
 _C.LOSS.OHEMTHRES = 0.9
 _C.LOSS.OHEMKEEP = 100000
 _C.LOSS.CLASS_BALANCE = True
 
 # DATASET related params
 _C.DATASET = CN()
-_C.DATASET.ROOT = ''
+_C.DATASET.ROOT = '' ## data root dir, ex:'data/'
 _C.DATASET.DATASET = 'cityscapes'
-_C.DATASET.NUM_CLASSES = 19
+_C.DATASET.NUM_CLASSES = 19  ## label counts
 _C.DATASET.TRAIN_SET = 'list/cityscapes/train.lst'
-_C.DATASET.EXTRA_TRAIN_SET = ''
+_C.DATASET.EXTRA_TRAIN_SET = ''  ##有一個額外功能, 可以設定model訓練到某epoch轉成訓練另一批training data, 這邊就是第二批data dir
 _C.DATASET.TEST_SET = 'list/cityscapes/val.lst'
 
 # training
 _C.TRAIN = CN()
 
 _C.TRAIN.IMAGE_SIZE = [1024, 512]  # width * height
-_C.TRAIN.BASE_SIZE = 2048
+_C.TRAIN.BASE_SIZE = 2048  ## used in multi_scale_aug , Notice to set a a proper number 
 _C.TRAIN.DOWNSAMPLERATE = 1
 _C.TRAIN.FLIP = True
 _C.TRAIN.MULTI_SCALE = True
@@ -68,18 +68,18 @@ _C.TRAIN.LR = 0.01
 _C.TRAIN.EXTRA_LR = 0.001
 
 _C.TRAIN.OPTIMIZER = 'sgd'
-_C.TRAIN.MOMENTUM = 0.9
+_C.TRAIN.MOMENTUM = 0.9 ## momentum on the optimiser
 _C.TRAIN.WD = 0.0001 ## weight decay rate
 _C.TRAIN.NESTEROV = False
 _C.TRAIN.IGNORE_LABEL = -1
 
 _C.TRAIN.BEGIN_EPOCH = 0
-_C.TRAIN.END_EPOCH = 484
+_C.TRAIN.END_EPOCH = 484  ## Notice 
 _C.TRAIN.EXTRA_EPOCH = 0
 
 _C.TRAIN.RESUME = False
 
-_C.TRAIN.BATCH_SIZE_PER_GPU = 32
+_C.TRAIN.BATCH_SIZE_PER_GPU = 32  ## Notice
 _C.TRAIN.SHUFFLE = True
 # only using some training samples
 _C.TRAIN.NUM_SAMPLES = 0
@@ -88,7 +88,7 @@ _C.TRAIN.NUM_SAMPLES = 0
 _C.TEST = CN()
 
 _C.TEST.IMAGE_SIZE = [2048, 1024]  # width * height
-_C.TEST.BASE_SIZE = 2048
+_C.TEST.BASE_SIZE = 2048 ## used in multi_scale_aug , Notice to set a a proper number 
 
 _C.TEST.BATCH_SIZE_PER_GPU = 32
 # only testing some samples
