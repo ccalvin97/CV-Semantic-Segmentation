017c7b70fa2d017a1d59344a4cf692fe5d10e523
Ubuntu <GDDC-CV1@GDDC-CV1.eegg2rbugghunj3lt1pat4s5jh.shax.internal.chinacloudapp.cn>
2021-01-28 07:33:19 +0000

    update

---
+++ HRNet-Semantic-Segmentation/start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+## Start for HRNet ##
+
+python tools/train.py \
+--cfg /home/GDDC-CV1/Desktop/CV-Semantic-Segmentation/HRNet-Semantic-Segmentation/experiments/seg_hrnet_w18_small_v1_512x1024_sgd_lr1e-2_wd5e-4_bs_12_epoch484.yaml
+
