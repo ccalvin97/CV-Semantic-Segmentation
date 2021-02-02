093cba44c532984cfda0dd0746d747695bd6aa84
ccalvin97 <41994182+ccalvin97@users.noreply.github.com>
2021-02-02 15:03:05 +0800

    Update train.py

--- HRNet-Semantic-Segmentation/tools/train.py
+++ HRNet-Semantic-Segmentation/tools/train.py
@@ -221,6 +221,13 @@ def main():
         }, os.path.join(final_output_dir,'checkpoint.pth.tar'))
         valid_loss, mean_IoU, IoU_array, tn_rate, tp_rate = validate(
                         config, testloader, model, writer_dict)
+        ‘’‘
+        Confusion Matrix = [[a,b], [c,d]]
+        IOU Array = [A, B]
+        A = a / (a+b+c)
+        B = d / (b+c+d)
+        ’‘’
+        
         if mean_IoU > best_mIoU:
             best_mIoU = mean_IoU
             torch.save(model.module.state_dict(),
